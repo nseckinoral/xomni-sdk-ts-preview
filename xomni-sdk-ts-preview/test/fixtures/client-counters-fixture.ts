7b806935853d1b024090070f3692741d87296cf6
Gokhan Gulbiz <ggulbiz@gmail.com>
2015-02-19 16:29:13 +0200

    Client-counters.ts unit tests implemented.

--- xomni-sdk-ts-preview/test/fixtures/client-counters-fixture.ts
+++ xomni-sdk-ts-preview/test/fixtures/client-counters-fixture.ts
@@ -1,22 +1,57 @@
-﻿/// <reference path="../../src/client-counters.ts" />
-/// <reference path="../../definitions/jasmine/jasmine.d.ts" />
+﻿/// <chutzpah_reference path="../../src/client-counters.ts" />
+/// <chutzpah_reference path="../../definitions/jasmine/jasmine.d.ts" />
 /// <chutzpah_reference path="../../definitions/jquery/jquery.d.ts" />
+/// <chutzpah_reference path="../testhelpers.ts" />
 
-describe('client-counters-fixture', () => {
-    it("Should username passed", () => {
-        Xomni.currentContext = new Xomni.ClientContext("mahmut", "password", "serviceUri");
-        expect(Xomni.currentContext.username).toEqual("mahmut");
+TestHelpers.InitalizeTestContext();
+
+describe('ClientCounterClient.getClientCounterList', () => {
+    it("Should hit correct url", () => {
+        TestHelpers.RequestUriTest($, "private/analytics/clientcounters");
+        var testClient = new Xomni.Private.Analytics.ClientCounters.ClientCounterClient();
+        testClient.getClientCounterList(suc => { }, err => { });
     });
 
-    it("Should hit correct url", () => {
-        Xomni.currentContext = new Xomni.ClientContext("userName", "password", "serviceUri");
-        spyOn($, "ajax")
-            .and
-            .callFake(params => {
-                expect(params.url).toContain("private/analytics/clientcounters");
-            });
+    it("Should use correct http method", () => {
+        TestHelpers.RequestHttpMethodTest($, "Get");
+        var testClient = new Xomni.Private.Analytics.ClientCounters.ClientCounterClient();
+        testClient.getClientCounterList(suc => { }, err => { });
+
+    });
+
+    it("Should use correct http headers", () => {
+        TestHelpers.RequestHttpHeadersTest($, {
+            "Authorization": "Basic " + btoa("u:p"),
+            "Accept": "application/vnd.xomni.api-v3_0, */*"
+        });
 
         var testClient = new Xomni.Private.Analytics.ClientCounters.ClientCounterClient();
         testClient.getClientCounterList(suc => { }, err => { });
+    });
+
+    it("Should parse response successfully", () => {
+        spyOn($, "ajax")
+            .and
+            .callFake(p => {
+                p.success({
+                    "ContinuationToken": "adqweqwlkasd12312dkslk",
+                    "CounterNames": [
+                        "categoryClicked",
+                        "itemClicked",
+                        "brandClicked"
+                    ]
+                }, null, null);
+            });
+        
+        var expectedSuccess = (counters: Xomni.Private.Analytics.ClientCounters.ClientCounterListContainer) => {
+            expect(counters.ContinuationToken).toEqual("adqweqwlkasd12312dkslk");
+            expect(counters.CounterNames.length).toEqual(3);
+            expect(counters.CounterNames[0]).toEqual("categoryClicked");
+            expect(counters.CounterNames[1]).toEqual("itemClicked");
+            expect(counters.CounterNames[2]).toEqual("brandClicked");
+        };
+
+        var testClient = new Xomni.Private.Analytics.ClientCounters.ClientCounterClient();
+        testClient.getClientCounterList(expectedSuccess, err => { });
     });
 });
