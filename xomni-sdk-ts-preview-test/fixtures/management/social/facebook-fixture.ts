af21bac6538c4f6e01b4060c591a1acfeb3a8836
Sezgin Ege <mail@sezginege.com>
2015-03-06 12:25:03 +0200

    FacebookClient and FacebookClientFixture added.

---
+++ xomni-sdk-ts-preview-test/fixtures/management/social/facebook-fixture.ts
@@ -0,0 +1,35 @@
+﻿TestHelpers.InitalizeTestContext();
+
+describe('Facebook.FacebookClient.getClientCounterList', () => {
+    it("Should hit correct url", () => {
+        TestHelpers.RequestUriTest($, "/management/social/facebookdisplaytypes");
+        var testClient = new Xomni.Management.Social.Facebook.FacebookClient();
+        testClient.getFacebookDisplayTypes(suc => { }, err => { });
+    });
+
+    it("Should use correct http method", () => {
+        TestHelpers.RequestHttpMethodTest($, "Get");
+        var testClient = new Xomni.Management.Social.Facebook.FacebookClient();
+        testClient.getFacebookDisplayTypes(suc => { }, err => { });
+
+    });
+
+    it("Should use correct http headers", () => {
+        TestHelpers.RequestHttpHeadersTest($);
+        var testClient = new Xomni.Management.Social.Facebook.FacebookClient();
+        testClient.getFacebookDisplayTypes(suc => { }, err => { });
+    });
+
+    it("Should parse response successfully", () => {
+        TestHelpers.ResponseParseTest($, { '0': 'Page', '1': 'Popup', '2': 'Touch' });
+
+        var expectedSuccess = (types: string)=> {
+            expect(types["0"]).toEqual('Page');
+            expect(types["1"]).toEqual('Popup');
+            expect(types["2"]).toEqual('Touch');
+        };
+
+        var testClient = new Xomni.Management.Social.Facebook.FacebookClient();
+        testClient.getFacebookDisplayTypes(expectedSuccess, err => { });
+    });
+});
---
+++ xomni-sdk-ts-preview/src/management/social/facebook.ts
@@ -0,0 +1,11 @@
+﻿/// <reference path="../../xomni.ts" />
+module Xomni.Management.Social.Facebook {
+    export class FacebookClient extends BaseClient {
+        private uri: string = "/management/social/facebookdisplaytypes";
+        
+        getFacebookDisplayTypes(success: (result: string) => void, error: (error: any) => void) {
+            this.httpProvider.get(this.uri, success, error);
+        }
+    }
+}
+    
