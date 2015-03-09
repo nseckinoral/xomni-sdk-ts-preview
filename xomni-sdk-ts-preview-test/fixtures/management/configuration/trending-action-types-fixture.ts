3b5ba2c01b4780f6b7a3ca5e30b8c49e63958c0d
Sezgin Ege <mail@sezginege.com>
2015-03-09 16:08:55 +0200

    updateTrendingActionTypeValues method of TrendingActionTypesClient implemented.

---
+++ xomni-sdk-ts-preview-test/fixtures/management/configuration/trending-action-types-fixture.ts
@@ -0,0 +1,55 @@
+﻿TestHelpers.InitalizeTestContext();
+
+var samplePutRequest: Array<Xomni.Management.Configuration.TrendingActionTypes.TrendingActionTypeContainer> = [{ Id: 1, Description: "SocialLike", ImpactValue: 5.1 }];
+
+describe('TrendingActionTypesClient.updateTrendingActionTypeValues', () => {
+
+    it("Should hit correct url", () => {
+        TestHelpers.RequestUriTest($, "/management/configuration/tenantactiontypes");
+        var testClient = new Xomni.Management.Configuration.TrendingActionTypes.TrendingActionTypesClient;
+        testClient.updateTrendingActionTypeValues(samplePutRequest, suc => { }, err => { });
+    })
+
+
+    it("Should use correct http method", () => {
+        TestHelpers.RequestHttpMethodTest($, "Put");
+        var testClient = new Xomni.Management.Configuration.TrendingActionTypes.TrendingActionTypesClient();
+        testClient.updateTrendingActionTypeValues(samplePutRequest,suc => { }, err => { });
+
+    });
+
+    it("Should use correct http headers", () => {
+        TestHelpers.RequestHttpHeadersTest($);
+
+        var testClient = new Xomni.Management.Configuration.TrendingActionTypes.TrendingActionTypesClient();
+        testClient.updateTrendingActionTypeValues(samplePutRequest,suc => { }, err => { });
+    });
+
+    it("Should parse response successfully", () => {
+        TestHelpers.ResponseParseTest($,
+            [
+                {
+                    "Id": 1,
+                    "Description": "SocialLike",
+                    "ImpactValue": 5.1
+                }
+            ]
+        );
+
+        var expectedSuccess = (trendingActionTypes: Xomni.Management.Configuration.TrendingActionTypes.TrendingActionTypeContainer[]) => {
+            expect(trendingActionTypes[0].Id).toEqual(1);
+            expect(trendingActionTypes[0].Description).toEqual("SocialLike");
+            expect(trendingActionTypes[0].ImpactValue).toEqual(5.1);
+        };
+
+        var testClient = new Xomni.Management.Configuration.TrendingActionTypes.TrendingActionTypesClient();
+        testClient.updateTrendingActionTypeValues(samplePutRequest, expectedSuccess, err=> { });
+    });
+
+    it("Should parse request successfully", () => {
+        TestHelpers.RequestParseTest($, [{ 'Id': 1, 'Description': 'SocialLike', 'ImpactValue': 5.1 }]);
+
+        var testClient = new Xomni.Management.Configuration.TrendingActionTypes.TrendingActionTypesClient();
+        testClient.updateTrendingActionTypeValues(samplePutRequest, suc=> { }, err=> { });
+    });
+});
---
+++ xomni-sdk-ts-preview/src/management/configuration/trending-action-types.ts
@@ -0,0 +1,16 @@
+﻿/// <reference path="../../xomni.ts" />
+module Xomni.Management.Configuration.TrendingActionTypes {
+    export class TrendingActionTypesClient extends BaseClient {
+        private uri: string = "/management/configuration/tenantactiontypes";
+
+        updateTrendingActionTypeValues(actionTypes: TrendingActionTypeContainer[], success: (result: TrendingActionTypeContainer[]) => void, error: (error: any) => void) {
+            this.httpProvider.put(this.uri, actionTypes, success, error);
+        }
+    }
+
+    export interface TrendingActionTypeContainer {
+        Id: number;
+        Description: string;
+        ImpactValue: number;
+    }
+}
