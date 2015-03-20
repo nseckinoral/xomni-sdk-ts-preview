6d1a3a8161c2ae8e6e7db9fd177f65087e933afa
Sezgin Ege <mail@sezginege.com>
2015-03-20 12:17:12 +0200

    get method of PrivateApiUserClient implemented.

--- xomni-sdk-ts-preview-test/fixtures/management/security/private-api-user-fixture.ts
+++ xomni-sdk-ts-preview-test/fixtures/management/security/private-api-user-fixture.ts
@@ -1,7 +1,9 @@
 ﻿TestHelpers.InitalizeTestContext();
+var validPrivateApiUserId: number = 2;
 var validSkip: number = 1;
 var validTake: number = 2;
 var validUriForGetList: string = "/management/security/privateapiusers?skip=" + validSkip + "&take=" + validTake;
+var validUri: string = "/management/security/privateapiuser/" + validPrivateApiUserId;
 
 describe('PrivateApiUserClient.getList', () => {
     it("Should hit correct url", () => {
@@ -99,5 +101,82 @@ describe('PrivateApiUserClient.getList', () => {
 
         expect(() => { testClient.getList(validSkip, -1, suc => { }, err => { }) })
             .toThrow(new Error("take must be greater than or equal to 1"));
+    });
+});
+
+describe('PrivateApiUserClient.get', () => {
+    it("Should hit correct url", () => {
+        TestHelpers.RequestUriTest($, validUri);
+        var testClient = new Xomni.Management.Security.PrivateApiUser.PrivateApiUserClient();
+        testClient.get(validPrivateApiUserId, suc => { }, err => { });
+    })
+
+    it("Should use correct http method", () => {
+        TestHelpers.RequestHttpMethodTest($, "Get");
+        var testClient = new Xomni.Management.Security.PrivateApiUser.PrivateApiUserClient();
+        testClient.get(validPrivateApiUserId, suc => { }, err => { });
+
+    });
+
+    it("Should use correct http headers", () => {
+        TestHelpers.RequestHttpHeadersTest($);
+
+        var testClient = new Xomni.Management.Security.PrivateApiUser.PrivateApiUserClient();
+        testClient.get(validPrivateApiUserId, suc => { }, err => { });
+    });
+
+    it("Should parse response successfully", () => {
+        TestHelpers.ResponseParseTest($, {
+            "Id": 9,
+            "Username": "NewPrivateApiUserName",
+            "Name": "New Private Api User",
+            "Password": "MyPassword",
+            "StoreId": null,
+            "Rights": [
+                {
+                    "Id": 2,
+                    "Description": "PrivateAPI"
+                }
+            ]
+        });
+
+        var expectedSuccess = (user: Models.Management.Security.ApiUser) => {
+            expect(user.Id).toEqual(9);
+            expect(user.Username).toEqual("NewPrivateApiUserName");
+            expect(user.Name).toEqual("New Private Api User");
+            expect(user.Password).toEqual("MyPassword");
+            expect(user.StoreId).toEqual(null);
+            expect(user.Rights.length).toEqual(1);
+            expect(user.Rights[0].Id).toEqual(2);
+            expect(user.Rights[0].Description).toEqual("PrivateAPI");
+        };
+
+        var testClient = new Xomni.Management.Security.PrivateApiUser.PrivateApiUserClient();
+        testClient.get(validPrivateApiUserId, expectedSuccess, err=> { });
+    });
+
+    it("Should raise exception with invalid parameters", () => {
+        var testClient = new Xomni.Management.Security.PrivateApiUser.PrivateApiUserClient();
+        expect(() => { testClient.get(null, suc => { }, err => { }) })
+            .toThrow(new Error("privateApiUserId could not be null or empty"));
+
+        expect(() => { testClient.get(undefined, suc => { }, err => { }) })
+            .toThrow(new Error("privateApiUserId could not be null or empty"));
+
+        expect(() => { testClient.get(-1, suc => { }, err => { }) })
+            .toThrow(new Error("privateApiUserId must be greater than or equal to 0"));
+    });
+
+    it("Should parse api exception response successfully", () => {
+        TestHelpers.APIExceptionResponseTest($, 404);
+
+        var expectedError = (exception: Models.ExceptionResult) => {
+            expect(exception.HttpStatusCode).toEqual(404);
+            expect(exception.FriendlyDescription).toEqual("Generic error friendly description.");
+            expect(exception.IdentifierGuid).toEqual("7358fe16-3925-4951-9a77-fca4f9e167b0");
+            expect(exception.IdentifierTick).toEqual(635585478999549713);
+        };
+        var testClient = new Xomni.Management.Security.PrivateApiUser.PrivateApiUserClient();
+        testClient.get(validPrivateApiUserId, suc=> { }, expectedError);
     });
 });
--- xomni-sdk-ts-preview/src/management/security/private-api-user.ts
+++ xomni-sdk-ts-preview/src/management/security/private-api-user.ts
@@ -2,6 +2,7 @@
 module Xomni.Management.Security.PrivateApiUser {
     export class PrivateApiUserClient extends BaseClient {
         private listOperationBaseUrl: string = "/management/security/privateapiusers";
+        private singleOperationBaseUrl: string = "/management/security/privateapiuser/";
 
         getList(skip: number, take: number, success: (result: Models.PaginatedContainer<Models.Management.Security.ApiUser>) => void, error: (error: Models.ExceptionResult) => void) {
             Xomni.Utils.Validator.isGreaterThanOrEqual("skip", skip, 0);
@@ -10,6 +11,12 @@ module Xomni.Management.Security.PrivateApiUser {
                 { key: "skip", value: skip.toString() },
                 { key: "take", value: take.toString() }
             ]));
+            this.httpProvider.get(uri, success, error);
+        }
+
+        get(privateApiUserId: number, success: (result: Models.Management.Security.ApiUser) => void, error: (error: Models.ExceptionResult) => void) {
+            Xomni.Utils.Validator.isGreaterThanOrEqual("privateApiUserId", privateApiUserId, 0);
+            var uri: string = Xomni.Utils.UrlGenerator.PrepareSingleOperationUrl(this.singleOperationBaseUrl, privateApiUserId.toString());
             this.httpProvider.get(uri, success, error);
         }
     }
