d3804f49d2232099a6b3ab7743d20c1301e42409
Gokhan Gulbiz <ggulbiz@gmail.com>
2015-03-20 11:55:48 +0200

    get method of StoreClient implemented.

---
+++ xomni-sdk-ts-preview-test/fixtures/management/configuration/store-fixture.ts
@@ -0,0 +1,92 @@
+﻿TestHelpers.InitalizeTestContext();
+var validStoreId: number = 49720;
+var validUri: string = "/management/configuration/store/" + validStoreId;
+
+describe('StoreClient.get', () => {
+    it("Should hit correct url", () => {
+        TestHelpers.RequestUriTest($, validUri);
+        var testClient = new Xomni.Management.Configuration.Store.StoreClient();
+        testClient.get(validStoreId, suc => { }, err => { });
+    });
+
+    it("Should use correct http method", () => {
+        TestHelpers.RequestHttpMethodTest($, "Get");
+        var testClient = new Xomni.Management.Configuration.Store.StoreClient();
+        testClient.get(validStoreId, suc => { }, err => { });
+    });
+
+    it("Should use correct http headers", () => {
+        TestHelpers.RequestHttpHeadersTest($);
+        var testClient = new Xomni.Management.Configuration.Store.StoreClient();
+        testClient.get(validStoreId, suc => { }, err => { });
+    });
+
+    it("Should raise exception with invalid parameters", () => {
+        var testClient = new Xomni.Management.Configuration.Store.StoreClient();
+        
+        expect(() => { testClient.get(-1, suc => { }, err => { }) })
+            .toThrow(new Error("storeId could not be less than 0."));
+
+        expect(() => { testClient.get(null, suc => { }, err => { }) })
+            .toThrow(new Error("storeId could not be null or empty."));
+
+        expect(() => { testClient.get(undefined, suc => { }, err => { }) })
+            .toThrow(new Error("storeId could not be null or empty."));
+    });
+
+    it("Should parse response successfully", () => {
+        TestHelpers.ResponseParseTest($, {
+            "Id": 1004,
+            "Name": "Relationed Store",
+            "Description": "The Description",
+            "Address": "Sample Data Address",
+            "Location": {
+                "Longitude": 23.54,
+                "Latitude": 35.41
+            },
+            "Licenses": [
+                {
+                    "Id": 3,
+                    "Username": "DebugApiManagementTestUser",
+                    "Name": "Debug Management Api User",
+                    "Password": "DebugApiManagementTestPass",
+                    "StoreId": 1004
+                }
+            ]
+        });
+
+        var expectedSuccess = (store: Models.Management.Configuration.Store) => {
+            expect(store.Id).toEqual(1004);
+            expect(store.Name).toEqual("Relationed Store");
+            expect(store.Description).toEqual("The Description");
+            expect(store.Address).toEqual("Sample Data Address");
+            expect(store.Location.Longitude).toEqual(23.54);
+            expect(store.Location.Latitude).toEqual(35.41);
+            expect(store.Licenses[0].Id).toEqual(3);
+            expect(store.Licenses[0].Username).toEqual("DebugApiManagementTestUser");
+            expect(store.Licenses[0].Name).toEqual("Debug Management Api User");
+            expect(store.Licenses[0].Password).toEqual("DebugApiManagementTestPass");
+            expect(store.Licenses[0].StoreId).toEqual(1004);
+        };
+
+        var testClient = new Xomni.Management.Configuration.Store.StoreClient();
+        testClient.get(validStoreId, expectedSuccess, err => { });
+    });
+
+    it("Should parse api exception response successfully", () => {
+        TestHelpers.APIExceptionResponseTest($, 404);
+
+        var expectedError = (exception: Models.ExceptionResult) => {
+            expect(exception.HttpStatusCode).toEqual(404);
+            expect(exception.FriendlyDescription).toEqual("Generic error friendly description.");
+            expect(exception.IdentifierGuid).toEqual("7358fe16-3925-4951-9a77-fca4f9e167b0");
+            expect(exception.IdentifierTick).toEqual(635585478999549713);
+        };
+
+        var testClient = new Xomni.Management.Configuration.Store.StoreClient();
+        testClient.get(validStoreId, suc=> { }, expectedError);
+    });
+});
+
+
+
---
+++ xomni-sdk-ts-preview/src/management/configuration/store.ts
@@ -0,0 +1,26 @@
+﻿/// <reference path="../../models/management/configuration/store.ts" />
+/// <reference path="../../xomni.ts" />
+module Xomni.Management.Configuration.Store {
+    export class StoreClient extends BaseClient {
+        private singleOperationBaseUrl: string = "/management/configuration/store/";
+
+        get(storeId: number, success: (result: Models.Management.Configuration.Store) => void, error: (error: Models.ExceptionResult) => void) {
+            this.ValidateStoreId(storeId);
+            var uri: string = this.PrepareSingleOperationUrl(storeId);
+            this.httpProvider.get(uri, success, error);
+        }
+
+        PrepareSingleOperationUrl(storeId: number): string {
+            return this.singleOperationBaseUrl + storeId;
+        }
+
+        ValidateStoreId(storeId: number): void {
+            if (storeId != 0 && !storeId) {
+                throw new Error("storeId could not be null or empty.");
+            }
+            if (storeId < 0) {
+                throw new Error("storeId could not be less than 0.");
+            }
+        }
+    }
+}
