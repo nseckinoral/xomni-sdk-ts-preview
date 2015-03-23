2a86a9cd7b20a892f0be1e8eca6c43de97b12979
Sezgin Ege <mail@sezginege.com>
2015-03-23 10:22:06 +0200

    Response parse test of FacebookClient updated.

--- xomni-sdk-ts-preview-test/fixtures/management/social/facebook-fixture.ts
+++ xomni-sdk-ts-preview-test/fixtures/management/social/facebook-fixture.ts
@@ -21,12 +21,18 @@ describe('Facebook.FacebookClient.getClientCounterList', () => {
     });
 
     it("Should parse response successfully", () => {
-        TestHelpers.ResponseParseTest($, { '0': 'Page', '1': 'Popup', '2': 'Touch' });
+        TestHelpers.ResponseParseTest($, {
+            "0": "Page",
+            "1": "Popup",
+            "2": "Touch"
+        });
 
-        var expectedSuccess = (types: string)=> {
-            expect(types["0"]).toEqual('Page');
-            expect(types["1"]).toEqual('Popup');
-            expect(types["2"]).toEqual('Touch');
+        var expectedSuccess = (types: Xomni.Dictionary<string,string>) => {
+            expect(types.keys().length).toEqual(3);
+            expect(types.values().length).toEqual(3);
+            expect(types.values()[0]).toEqual("Page");
+            expect(types.values()[1]).toEqual("Popup");
+            expect(types.values()[2]).toEqual("Touch");
         };
 
         var testClient = new Xomni.Management.Social.Facebook.FacebookClient();
