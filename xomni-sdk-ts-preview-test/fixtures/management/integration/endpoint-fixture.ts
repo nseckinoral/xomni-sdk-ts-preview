9ef9c1b70a47e47f4d116a0c2ee21610e942baf0
Gokhan Gulbiz <ggulbiz@gmail.com>
2015-03-26 15:49:08 +0200

    Added Management-Integration-Endpoint Client get method.

---
+++ xomni-sdk-ts-preview-test/fixtures/management/integration/endpoint-fixture.ts
@@ -0,0 +1,42 @@
+﻿TestHelpers.InitalizeTestContext();
+
+var validResponse = {
+    "ServiceName": "test",
+    "ManagementPortalUrl": "http://xomni.com"
+};
+
+describe('EndpointClient.get', () => {
+    it("Should hit correct url", () => {
+        TestHelpers.RequestUriTest($, "/management/integration/endpoint");
+
+        var testClient = new Xomni.Management.Integration.Endpoint.EndpointClient();
+        testClient.get(suc => { }, err => { });
+    });
+
+    it("Should use correct http method", () => {
+        TestHelpers.RequestHttpMethodTest($, "Get");
+
+        var testClient = new Xomni.Management.Integration.Endpoint.EndpointClient();
+        testClient.get(suc => { }, err => { });
+    });
+
+    it("Should use correct http headers", () => {
+        TestHelpers.RequestHttpHeadersTest($);
+
+        var testClient = new Xomni.Management.Integration.Endpoint.EndpointClient();
+        testClient.get(suc => { }, err => { });
+    });
+
+    it("Should parse response successfully", () => {
+        TestHelpers.ResponseParseTest($, validResponse);
+
+        var testClient = new Xomni.Management.Integration.Endpoint.EndpointClient();
+        testClient.get(
+            (r: Models.Management.Integration.EndpointDetail) => {
+                expect(r.ServiceName).toEqual("test");
+                expect(r.ManagementPortalUrl).toEqual("http://xomni.com");
+            },
+            err => {
+        });
+    });
+});
---
+++ xomni-sdk-ts-preview/src/management/integration/endpoint.ts
@@ -0,0 +1,11 @@
+﻿/// <reference path="../../xomni.ts" />
+/// <reference path="../../models/management/integration/endpoint-detail.ts" />
+module Xomni.Management.Integration.Endpoint {
+    export class EndpointClient extends BaseClient {
+        private uri: string = "/management/integration/endpoint";
+
+        get(success: (result: Models.Management.Integration.EndpointDetail) => void, error: (error: Models.ExceptionResult) => void) {
+            this.httpProvider.get(this.uri, success, error);
+        }
+    }
+}
