99f7423ea46ce53ef684b711a932c2c89a43b72d
Sezgin Ege <mail@sezginege.com>
2015-03-30 12:53:00 +0300

    getList method of LicenseClient implemented.

--- xomni-sdk-ts-preview-test/fixtures/management/security/license-fixture.ts
+++ xomni-sdk-ts-preview-test/fixtures/management/security/license-fixture.ts
@@ -2,12 +2,12 @@
 var validSkip: number = 1;
 var validTake: number = 2;
 var validLicenseId: number = 1;
-var validUri: string = "/management/security/license/" + validLicenseId;
+var validUriForGet: string = "/management/security/license/" + validLicenseId;
 var validUriForGetList = "/management/security/licenses?skip=" + validSkip + "&take=" + validTake;;
 
 describe('LicenseClient.get', () => {
     it("Should hit correct url", () => {
-        TestHelpers.RequestUriTest($, validUri);
+        TestHelpers.RequestUriTest($, validUriForGet);
         var testClient = new Xomni.Management.Security.License.LicenseClient();
         testClient.get(validLicenseId, suc => { }, err => { });
     });
@@ -70,5 +70,88 @@ describe('LicenseClient.get', () => {
 
         var testClient = new Xomni.Management.Security.License.LicenseClient();
         testClient.get(validLicenseId, suc=> { }, expectedError);
+    });
+});
+
+describe('LicenseClient.getList', () => {
+    it("Should hit correct url", () => {
+        TestHelpers.RequestUriTest($, validUriForGetList);
+        var testClient = new Xomni.Management.Security.License.LicenseClient();
+        testClient.getList(validSkip, validTake, suc => { }, err => { });
+    });
+
+    it("Should use correct http method", () => {
+        TestHelpers.RequestHttpMethodTest($, "Get");
+        var testClient = new Xomni.Management.Security.License.LicenseClient();
+        testClient.getList(validSkip, validTake, suc => { }, err => { });
+    });
+
+    it("Should use correct http headers", () => {
+        TestHelpers.RequestHttpHeadersTest($);
+        var testClient = new Xomni.Management.Security.License.LicenseClient();
+        testClient.getList(validSkip, validTake, suc => { }, err => { });
+    });
+
+    it("Should raise exception with invalid parameters", () => {
+        var testClient = new Xomni.Management.Security.License.LicenseClient();
+
+        expect(() => { testClient.getList(null, validTake, suc => { }, err => { }) })
+            .toThrow(new Error("skip could not be null or empty"));
+
+        expect(() => { testClient.getList(undefined, validTake, suc => { }, err => { }) })
+            .toThrow(new Error("skip could not be null or empty"));
+
+        expect(() => { testClient.getList(-5, validTake, suc => { }, err => { }) })
+            .toThrow(new Error("skip must be greater than or equal to 0"));
+
+        expect(() => { testClient.getList(validSkip, null, suc => { }, err => { }) })
+            .toThrow(new Error("take could not be null or empty"));
+
+        expect(() => { testClient.getList(validSkip, undefined, suc => { }, err => { }) })
+            .toThrow(new Error("take could not be null or empty"));
+
+        expect(() => { testClient.getList(validSkip, -5, suc => { }, err => { }) })
+            .toThrow(new Error("take must be greater than or equal to 1"));
+    });
+
+    it("Should parse response successfully", () => {
+        TestHelpers.ResponseParseTest($, {
+            "Results": [
+                {
+                    "Id": 1,
+                    "Username": "DebugApiTestUser",
+                    "Name": "Debug Public Api User",
+                    "Password": "DebugApiTestPass",
+                    "StoreId": 56
+                },
+                {
+                    "Id": 4,
+                    "Username": "SampleLicenceUsername",
+                    "Name": "Sample Licence Record",
+                    "Password": "123",
+                    "StoreId": 78
+                }
+            ],
+            "TotalCount": 2
+        });
+
+        var expectedSuccess = (licenses: Models.PaginatedContainer<Models.Management.Security.License>) => {
+            expect(licenses.Results.length).toEqual(2);
+            expect(licenses.TotalCount).toEqual(2);
+            expect(licenses.Results[0].Id).toEqual(1);
+            expect(licenses.Results[0].Username).toEqual("DebugApiTestUser");
+            expect(licenses.Results[0].Name).toEqual("Debug Public Api User");
+            expect(licenses.Results[0].Password).toEqual("DebugApiTestPass");
+            expect(licenses.Results[0].StoreId).toEqual(56);
+
+            expect(licenses.Results[1].Id).toEqual(4);
+            expect(licenses.Results[1].Username).toEqual("SampleLicenceUsername");
+            expect(licenses.Results[1].Name).toEqual("Sample Licence Record");
+            expect(licenses.Results[1].Password).toEqual("123");
+            expect(licenses.Results[1].StoreId).toEqual(78);
+        };
+
+        var testClient = new Xomni.Management.Security.License.LicenseClient();
+        testClient.getList(validSkip, validTake, expectedSuccess, err => { });
     });
 });
--- xomni-sdk-ts-preview/src/management/security/license.ts
+++ xomni-sdk-ts-preview/src/management/security/license.ts
@@ -2,10 +2,21 @@
 module Xomni.Management.Security.License {
     export class LicenseClient extends BaseClient {
         private singleOperationBaseUrl: string = "/management/security/license/";
+        private listOperationBaseUrl: string = "/management/security/licenses";
 
         get(licenseId: number, success: (result: Models.Management.Security.License) => void, error: (error: Models.ExceptionResult) => void) {
             Xomni.Utils.Validator.isGreaterThanOrEqual("licenseId", licenseId, 0);
             var uri: string = Xomni.Utils.UrlGenerator.PrepareOperationUrl(this.singleOperationBaseUrl, licenseId.toString());
+            this.httpProvider.get(uri, success, error);
+        }
+
+        getList(skip: number, take: number, success: (result: Models.PaginatedContainer<Models.Management.Security.License>) => void, error: (error: Models.ExceptionResult) => void) {
+            Xomni.Utils.Validator.isGreaterThanOrEqual("skip", skip, 0);
+            Xomni.Utils.Validator.isGreaterThanOrEqual("take", take, 1);
+            var uri: string = Xomni.Utils.UrlGenerator.PrepareOperationUrlWithMultipleParameter(this.listOperationBaseUrl, new Dictionary<string, string>([
+                { key: "skip", value: skip.toString() },
+                { key: "take", value: take.toString() }
+            ]));
             this.httpProvider.get(uri, success, error);
         }
     }
