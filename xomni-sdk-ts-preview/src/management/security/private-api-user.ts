cee5a350b03fed9518ba7a2457b2d9d720003788
Gokhan Gulbiz <ggulbiz@gmail.com>
2015-03-30 12:44:39 +0300

    Fixed helper module methods name based bugs.

--- xomni-sdk-ts-preview/src/management/security/private-api-user.ts
+++ xomni-sdk-ts-preview/src/management/security/private-api-user.ts
@@ -7,7 +7,7 @@ module Xomni.Management.Security.PrivateApiUser {
         getList(skip: number, take: number, success: (result: Models.PaginatedContainer<Models.Management.Security.ApiUser>) => void, error: (error: Models.ExceptionResult) => void) {
             Xomni.Utils.Validator.isGreaterThanOrEqual("skip", skip, 0);
             Xomni.Utils.Validator.isGreaterThanOrEqual("take", take, 1);
-            var uri = Xomni.Utils.UrlGenerator.PrepareListOperationUrl(this.listOperationBaseUrl, new Dictionary<string, string>([
+            var uri = Xomni.Utils.UrlGenerator.PrepareOperationUrlWithMultipleParameter(this.listOperationBaseUrl, new Dictionary<string, string>([
                 { key: "skip", value: skip.toString() },
                 { key: "take", value: take.toString() }
             ]));
@@ -16,13 +16,13 @@ module Xomni.Management.Security.PrivateApiUser {
 
         get(privateApiUserId: number, success: (result: Models.Management.Security.ApiUser) => void, error: (error: Models.ExceptionResult) => void) {
             Xomni.Utils.Validator.isGreaterThanOrEqual("privateApiUserId", privateApiUserId, 0);
-            var uri: string = Xomni.Utils.UrlGenerator.PrepareSingleOperationUrl(this.singleOperationBaseUrl, privateApiUserId.toString());
+            var uri: string = Xomni.Utils.UrlGenerator.PrepareOperationUrl(this.singleOperationBaseUrl, privateApiUserId.toString());
             this.httpProvider.get(uri, success, error);
         }
 
         delete(privateApiUserId: number, success: () => void, error: (error: Models.ExceptionResult) => void) {
             Xomni.Utils.Validator.isGreaterThanOrEqual("privateApiUserId", privateApiUserId, 0);
-            var uri: string = Xomni.Utils.UrlGenerator.PrepareSingleOperationUrl(this.singleOperationBaseUrl, privateApiUserId.toString());
+            var uri: string = Xomni.Utils.UrlGenerator.PrepareOperationUrl(this.singleOperationBaseUrl, privateApiUserId.toString());
             this.httpProvider.delete(uri, success, error);
         }
 
