bf2ad3ed127f399f35de73bb46c861562bdf0835
Sezgin Ege <mail@sezginege.com>
2015-04-01 15:10:37 +0300

    put method of DeviceMetadataClient implemented.

--- xomni-sdk-ts-preview-test/fixtures/management/company/device-metadata-fixture.ts
+++ xomni-sdk-ts-preview-test/fixtures/management/company/device-metadata-fixture.ts
@@ -6,6 +6,16 @@ var metadata: Models.Management.Company.Metadata = {
     Value: "Sample metadata value"
 };
 
+var requestJson = {
+    "Key": "Sample metadata key",
+    "Value": "Sample metadata value"
+};
+
+var responseJson = {
+    "Key": "Sample metadata key",
+    "Value": "Sample metadata value"
+};
+
 describe('DeviceMetadataClient.post', () => {
     it("Should hit correct url", () => {
         TestHelpers.RequestUriTest($, validUri);
@@ -26,10 +36,7 @@ describe('DeviceMetadataClient.post', () => {
     });
 
     it("Should parse response successfully", () => {
-        TestHelpers.ResponseParseTest($, {
-            "Key": "Sample metadata key",
-            "Value": "Sample metadata value"
-        });
+        TestHelpers.ResponseParseTest($, responseJson);
 
         var expectedSuccess = (response: Models.Management.Company.Metadata) => {
             expect(response.Key).toEqual(metadata.Key);
@@ -42,10 +49,7 @@ describe('DeviceMetadataClient.post', () => {
 
     it("Should parse request successfully", () => {
         var parseMethod = (request: Models.Management.Company.Metadata) => {
-            expect(request).toEqual({
-                "Key": "Sample metadata key",
-                "Value": "Sample metadata value"
-            });
+            expect(request).toEqual(requestJson);
             expect(request.Key).toEqual(metadata.Key);
             expect(request.Value).toEqual(metadata.Value);
         };
@@ -137,5 +141,119 @@ describe('DeviceMetadataClient.post', () => {
 
         var testClient = new Xomni.Management.Company.DeviceMetadata.DeviceMetadataClient();
         testClient.post(validLicenseId, TestHelpers.uniqeId, metadata, suc=> { }, expectedError);
+    });
+});
+
+describe('DeviceMetadataClient.put', () => {
+    it("Should hit correct url", () => {
+        TestHelpers.RequestUriTest($, validUri);
+        var testClient = new Xomni.Management.Company.DeviceMetadata.DeviceMetadataClient();
+        testClient.put(validLicenseId, TestHelpers.uniqeId, metadata, suc => { }, err => { });
+    });
+
+    it("Should use correct http method", () => {
+        TestHelpers.RequestHttpMethodTest($, "Put");
+        var testClient = new Xomni.Management.Company.DeviceMetadata.DeviceMetadataClient();
+        testClient.put(validLicenseId, TestHelpers.uniqeId, metadata, suc => { }, err => { });
+    });
+
+    it("Should use correct http headers", () => {
+        TestHelpers.RequestHttpHeadersTest($);
+        var testClient = new Xomni.Management.Company.DeviceMetadata.DeviceMetadataClient();
+        testClient.put(validLicenseId, TestHelpers.uniqeId, metadata, suc => { }, err => { });
+    });
+
+    it("Should parse response successfully", () => {
+        TestHelpers.ResponseParseTest($, responseJson);
+
+        var expectedSuccess = (response: Models.Management.Company.Metadata) => {
+            expect(response.Key).toEqual(metadata.Key);
+            expect(response.Value).toEqual(metadata.Value);
+        };
+
+        var testClient = new Xomni.Management.Company.DeviceMetadata.DeviceMetadataClient();
+        testClient.put(validLicenseId, TestHelpers.uniqeId, metadata, expectedSuccess, err => { });
+    });
+
+    it("Should parse request successfully", () => {
+        var parseMethod = (request: Models.Management.Company.Metadata) => {
+            expect(request).toEqual(requestJson);
+            expect(request.Key).toEqual(metadata.Key);
+            expect(request.Value).toEqual(metadata.Value);
+        };
+
+        TestHelpers.RequestParseTest($, parseMethod);
+
+        var testClient = new Xomni.Management.Company.DeviceMetadata.DeviceMetadataClient();
+        testClient.put(validLicenseId, TestHelpers.uniqeId, metadata, suc=> { }, err => { });
+    });
+
+    it("Should raise exception with invalid parameters", () => {
+        var testClient = new Xomni.Management.Company.DeviceMetadata.DeviceMetadataClient();
+
+        expect(() => { testClient.put(null, TestHelpers.uniqeId, metadata, suc => { }, err => { }) })
+            .toThrow(new Error("licenceId could not be null or empty"));
+
+        expect(() => { testClient.put(undefined, TestHelpers.uniqeId, metadata, suc => { }, err => { }) })
+            .toThrow(new Error("licenceId could not be null or empty"));
+
+        expect(() => { testClient.put(validLicenseId, null, metadata, suc => { }, err => { }) })
+            .toThrow(new Error("deviceId could not be null or empty"));
+
+        expect(() => { testClient.put(validLicenseId, undefined, metadata, suc => { }, err => { }) })
+            .toThrow(new Error("deviceId could not be null or empty"));
+
+        expect(() => { testClient.put(validLicenseId, TestHelpers.uniqeId, null, suc => { }, err => { }) })
+            .toThrow(new Error("metadata could not be null or empty"));
+
+        expect(() => { testClient.put(validLicenseId, TestHelpers.uniqeId, undefined, suc => { }, err => { }) })
+            .toThrow(new Error("metadata could not be null or empty"));
+
+        expect(() => {
+            var invalidMetadata: Models.Management.Company.Metadata = {
+                Key: null,
+                Value: "Value"
+            };
+            testClient.put(validLicenseId, TestHelpers.uniqeId, invalidMetadata, suc => { }, err => { })
+        }).toThrow(new Error("key could not be null or empty"));
+
+        expect(() => {
+            var invalidMetadata: Models.Management.Company.Metadata = {
+                Key: undefined,
+                Value: "Value"
+            };
+            testClient.put(validLicenseId, TestHelpers.uniqeId, invalidMetadata, suc => { }, err => { })
+        }).toThrow(new Error("key could not be null or empty"));
+
+        expect(() => {
+            var invalidMetadata: Models.Management.Company.Metadata = {
+                Key: "Key",
+                Value: null
+            };
+            testClient.put(validLicenseId, TestHelpers.uniqeId, invalidMetadata, suc => { }, err => { })
+        }).toThrow(new Error("value could not be null or empty"));
+
+        expect(() => {
+            var invalidMetadata: Models.Management.Company.Metadata = {
+                Key: "Key",
+                Value: undefined
+            };
+            testClient.put(validLicenseId, TestHelpers.uniqeId, invalidMetadata, suc => { }, err => { })
+        }).toThrow(new Error("value could not be null or empty"));
+
+    });
+
+    it("Should parse api exception response successfully (404)", () => {
+        TestHelpers.APIExceptionResponseTest($, 404);
+
+        var expectedError = (exception: Models.ExceptionResult) => {
+            expect(exception.HttpStatusCode).toEqual(404);
+            expect(exception.FriendlyDescription).toEqual("Generic error friendly description.");
+            expect(exception.IdentifierGuid).toEqual("7358fe16-3925-4951-9a77-fca4f9e167b0");
+            expect(exception.IdentifierTick).toEqual(635585478999549713);
+        };
+
+        var testClient = new Xomni.Management.Company.DeviceMetadata.DeviceMetadataClient();
+        testClient.put(validLicenseId, TestHelpers.uniqeId, metadata, suc=> { }, expectedError);
     });
 });
--- xomni-sdk-ts-preview/src/management/company/device-metadata.ts
+++ xomni-sdk-ts-preview/src/management/company/device-metadata.ts
@@ -16,5 +16,19 @@ module Xomni.Management.Company.DeviceMetadata {
 
             this.httpProvider.post(uri, metadata, success, error);
         }
+
+        put(licenceId: number, deviceId: string, metadata: Models.Management.Company.Metadata, success: (result: Models.Management.Company.Metadata) => void, error: (error: Models.ExceptionResult) => void) {
+            Xomni.Utils.Validator.isGreaterThanOrEqual("licenceId", licenceId, 0);
+            Xomni.Utils.Validator.isDefined("deviceId", deviceId);
+            Xomni.Utils.Validator.isDefined("metadata", metadata);
+            Xomni.Utils.Validator.isDefined("key", metadata.Key);
+            Xomni.Utils.Validator.isDefined("value", metadata.Value);
+            var uri = Xomni.Utils.UrlGenerator.ReplaceUri(this.baseUri, new Dictionary<string, string>([
+                { key: "{licenceId}", value: licenceId.toString() },
+                { key: "{deviceId}", value: deviceId },
+            ]));
+
+            this.httpProvider.put(uri, metadata, success, error);
+        }
     }
 } 
