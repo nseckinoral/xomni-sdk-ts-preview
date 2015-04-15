b282977759825763a5172aaee685e70f50e86742
Sezgin Ege <mail@sezginege.com>
2015-04-15 14:58:11 +0300

    delete method of DeviceClient implemented.

---
+++ xomni-sdk-ts-preview-test/fixtures/management/company/device-fixture.ts
@@ -0,0 +1,56 @@
+﻿TestHelpers.InitalizeTestContext();
+var validrelatedLicenceId = 1;
+var validUri = "/management/company/devices/" + TestHelpers.uniqueId + "?relatedLicenceId=" + validrelatedLicenceId;
+
+describe('DeviceClient.delete', () => {
+    it("Should hit correct url", () => {
+        TestHelpers.RequestUriTest($, validUri);
+        var testClient = new Xomni.Management.Company.Device.DeviceClient();
+        testClient.delete(TestHelpers.uniqueId, validrelatedLicenceId, () => { }, err => { });
+    });
+
+    it("Should use correct http method", () => {
+        TestHelpers.RequestHttpMethodTest($, "Delete");
+        var testClient = new Xomni.Management.Company.Device.DeviceClient();
+        testClient.delete(TestHelpers.uniqueId, validrelatedLicenceId, () => { }, err => { });
+    });
+
+    it("Should use correct http headers", () => {
+        TestHelpers.RequestHttpHeadersTest($);
+        var testClient = new Xomni.Management.Company.Device.DeviceClient();
+        testClient.delete(TestHelpers.uniqueId, validrelatedLicenceId, () => { }, err => { });
+    });
+
+    it("Should raise exception with invalid parameters", () => {
+        var testClient = new Xomni.Management.Company.Device.DeviceClient();
+
+        expect(() => { testClient.delete(null, validrelatedLicenceId, () => { }, err => { }) })
+            .toThrow(new Error("deviceId could not be null or empty"));
+
+        expect(() => { testClient.delete(undefined, validrelatedLicenceId, () => { }, err => { }) })
+            .toThrow(new Error("deviceId could not be null or empty"));
+
+        expect(() => { testClient.delete(TestHelpers.uniqueId, null, () => { }, err => { }) })
+            .toThrow(new Error("relatedLicenceId could not be null or empty"));
+
+        expect(() => { testClient.delete(TestHelpers.uniqueId, undefined, () => { }, err => { }) })
+            .toThrow(new Error("relatedLicenceId could not be null or empty"));
+
+        expect(() => { testClient.delete(TestHelpers.uniqueId, 0, () => { }, err => { }) })
+            .toThrow(new Error("relatedLicenceId must be greater than or equal to 1"));
+    });
+
+    it("Should parse api exception response successfully", () => {
+        TestHelpers.APIExceptionResponseTest($, 404);
+
+        var expectedError = (exception: Models.ExceptionResult) => {
+            expect(exception.HttpStatusCode).toEqual(404);
+            expect(exception.FriendlyDescription).toEqual("Generic error friendly description.");
+            expect(exception.IdentifierGuid).toEqual("7358fe16-3925-4951-9a77-fca4f9e167b0");
+            expect(exception.IdentifierTick).toEqual(635585478999549713);
+        };
+
+        var testClient = new Xomni.Management.Company.Device.DeviceClient();
+        testClient.delete(TestHelpers.uniqueId, validrelatedLicenceId, () => { }, expectedError);
+    });
+});
---
+++ xomni-sdk-ts-preview/src/management/company/device-client.ts
@@ -0,0 +1,17 @@
+﻿/// <reference path="../../xomni.ts" />
+module Xomni.Management.Company.Device {
+    export class DeviceClient extends BaseClient {
+        private baseUri = "/management/company/devices";
+
+        delete(deviceId: string, relatedLicenceId: number, success: () => void, error: (error: Models.ExceptionResult) => void) {
+            Xomni.Utils.Validator.isDefined("deviceId", deviceId);
+            Xomni.Utils.Validator.isGreaterThanOrEqual("relatedLicenceId", relatedLicenceId, 1);
+            var uri = Xomni.Utils.UrlGenerator.PrepareOperationUrl(this.baseUri, deviceId);
+            uri += Xomni.Utils.UrlGenerator.PrepareOperationUrlWithMultipleParameters(uri, new Dictionary<string, string>([
+                { key: "relatedLicenceId", value: relatedLicenceId.toString() }
+            ]));
+
+            this.httpProvider.delete(uri, success, error);
+        }
+    }
+}
