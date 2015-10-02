c3d0ddf7d116120fe86c6ddc8d06716f6c194d83
nseckinoral <nseckinoral@gmail.com>
2015-10-02 11:50:56 +0300

    Implemented exception-fixture to test Global Exception Handling

---
+++ xomni-sdk-ts-preview-test/fixtures/utils/exception-fixture.ts
@@ -0,0 +1,31 @@
+﻿describe('Raising exception', () => {
+    it("Should throw a proper exception", () => {
+        var expectedExceptionResult = new Models.ExceptionResult();
+        expectedExceptionResult.FriendlyDescription = "Generic error friendly description.";
+        expectedExceptionResult.IdentifierGuid = "7358fe16- 3925 - 4951 - 9a77- fca4f9e167b0";
+        expectedExceptionResult.IdentifierTick = 635585478999549713;
+
+        window.onerror = (event: any, source?: string, fileno?: number, columnNumber?: number, error?: any): void => {
+            if (error) {
+                expect(
+                    error.FriendlyDescription
+                    ).toEqual("Generic error friendly description.");
+                expect(
+                    error.IdentifierGuid
+                    ).toEqual("7358fe16- 3925 - 4951 - 9a77- fca4f9e167b0");
+                expect(
+                    error.IdentifierTick
+                    ).toEqual(635585478999549713);
+            }
+        };
+
+        try
+        {
+            throw expectedExceptionResult;
+        }
+        catch (error)
+        {
+            window.onerror.call(error);
+        }
+    })
+});
