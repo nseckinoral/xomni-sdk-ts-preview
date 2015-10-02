913162ec7c96fb97d528a88b9a426afcc85442da
nseckinoral <nseckinoral@gmail.com>
2015-10-02 14:07:40 +0300

    Fixed a bug where the error was undefined but still passed the tests.

--- xomni-sdk-ts-preview-test/fixtures/utils/exception-fixture.ts
+++ xomni-sdk-ts-preview-test/fixtures/utils/exception-fixture.ts
@@ -6,26 +6,23 @@
         expectedExceptionResult.IdentifierTick = 635585478999549713;
 
         window.onerror = (event: any, source?: string, fileno?: number, columnNumber?: number, error?: any): void => {
-            if (error) {
-                expect(
-                    error.FriendlyDescription
-                    ).toEqual("Generic error friendly description.");
-                expect(
-                    error.IdentifierGuid
-                    ).toEqual("7358fe16- 3925 - 4951 - 9a77- fca4f9e167b0");
-                expect(
-                    error.IdentifierTick
-                    ).toEqual(635585478999549713);
-            }
+            expect(error).toBeDefined();
+            expect(
+                error.FriendlyDescription
+                ).toEqual("Generic error friendly description.");
+            expect(
+                error.IdentifierGuid
+                ).toEqual("7358fe16- 3925 - 4951 - 9a77- fca4f9e167b0");
+            expect(
+                error.IdentifierTick
+                ).toEqual(635585478999549713);
         };
 
-        try
-        {
+        try {
             throw expectedExceptionResult;
         }
-        catch (error)
-        {
-            window.onerror.call(error);
+        catch (error) {
+            window.onerror.call(this,"","",null,null,error);
         }
     })
 });
